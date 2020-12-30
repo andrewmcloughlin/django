a1f68361c82c2f381e928c8314f8e244a30880e0
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2020-12-30 14:26:22 +0000

    refactored

--- django-app-wizard.sh
+++ django-app-wizard.sh
@@ -29,64 +29,111 @@ then
    helpFunction
 fi
 
-# Begin script in case all parameters are correct
-echo -e "\n${bold}creating an app called $appName${normal}"
+# Begin script if all parameters are correct
 
+models_py="
+from django.db import models
+
+class $modelName(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=4000)
+    def __str__(self):
+         return self.title
+"
+views_py="
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from $modelName.models import $modelName
+from $modelName.forms import modelNameForm
+
+def list_view(request):
+    all_objects = $modelName.objects.all()
+    context = {
+        'all_objects': all_objects
+    }
+    return render(request, '$modelName/index.html', context)
+
+def main_form(request):
+    form = mainForm()
+    if request.method == 'POST':
+        form = mainForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = mainForm()
+    context = {
+        'form': form
+    }
+    return render(request, '$modelName/add.html', context)
+"
+forms_py="
+from django.forms import ModelForm
+from $modelName.models import $modelName
+
+class modelNameForm(ModelForm):
+    class Meta:
+        model = $modelName
+        fields = '__all__'
+"
+
+admin_text="
+from django.contrib import admin
+from .models import testModel
+
+admin.site.register(testModel)
+"
+
+# set up venv
 echo -e "\n${bold}setting up a virtual environment${normal}"
-rm -rf testApp
+cp -R ~/dev/tyto/python3-venv/ ~/dev/personal-python3-venv    #replace with maske venv
+source personal-python3-venv/bin/activate
+
+# check django version
+echo -e "\n${bold}You are running django version:{normal}"
+python3 -m pip install -U Django
+python3 -m django --version
+
+# create app and remove previous version
+echo -e "\n${bold}creating an app called $appName${normal}"
+rm -rf $appName
 django-admin startproject $appName
-cp -R ~/dev/tyto/python3-venv/ $appName/python3-venv
-source $appName/python3-venv/bin/activate
-cd $appName
 
-
-#startapp
+# startapp and create model
 echo -e "\n${bold}creating a model called $modelName${normal}"
+cd $appName
 python3 manage.py startapp $modelName
-echo -e "class $modelName(models.Model): \n    title = models.CharField(max_length=200) \n    description = models.CharField(max_length=4000)\n    def __str__(self):\n        return self.title" >> ~/dev/$appName/$modelName/models.py
+echo -e "$models_py" > ~/dev/$appName/$modelName/models.py
 
-#modify view
-echo -e "\n${bold}creating default view${normal}"
-echo -e "from django.http import HttpResponse\n\ndef index(request):\n    return HttpResponse('Hello, world.')" >> ~/dev/$appName/$modelName/views.py
-
-#create a file called urls.py in the project directory
-echo -e "from django.urls import path\nfrom . import views\n\nurlpatterns = [\n    path('', views.index, name='index'),\n]" >> ~/dev/$appName/$modelName/urls.py
-
-#add url to 
-echo -e "\n${bold}modify urls.py${normal}"
-import_list="from django.urls import include, path"
-sed -i "s/^from django\.urls import path.*/${import_list}/" ~/dev/$appName/$appName/urls.py
-url_patterns="urlpatterns = [\n    path('', include('testModel.urls')),\n    path('testModel\/', include('testModel.urls')),"
-sed -i "s/^urlpatterns.*/${url_patterns}/" ~/dev/$appName/$appName/urls.py
-
-
-echo -e "\n${bold}modifying allowed hosts${normal}"
-allowed_hosts="ALLOWED_HOSTS = ['andrewmcloughlin.pythonanywhere.com','127.0.0.1']"
-sed -i "s/^ALLOWED_HOSTS.*/${allowed_hosts}/" ~/dev/$appName/$appName/settings.py
-
+# add app to installed apps
 echo -e "\n${bold}installing the model${normal}"
-installed_apps="INSTALLED_APPS = [ \n    '${appName}',"
+installed_apps="INSTALLED_APPS = [\n    '${appName}',"
 sed -i "s/^INSTALLED_APPS.*/${installed_apps}/" ~/dev/$appName/$appName/settings.py
 
-#change db type -not sure if this is necessary
-# db_name="        'NAME': BASE_DIR \/ 'db.sqlite3',"
-# sed -i "s/^        'NAME': os.path.join.*/${db_name}/" ~/dev/$appName/$appName/settings.py
-
-
-echo -e "\n${bold}activating the model${normal}"
-python3 manage.py makemigrations $appName
-
-echo -e "\n${bold}add model to admin interface${normal}"
-echo -e "from .models import $modelName \n \nadmin.site.register($modelName)" >> ~/dev/$appName/$modelName/admin.py
-
-echo -e "\n${bold}making migrations${normal}"
+# make migrations
 python3 manage.py makemigrations $appName
 python3 manage.py migrate
 
+# Add a default view
+echo -e "$view_text" > ~/dev/$appName/$modelName/views.py
+
+# Add a form
+echo -e "$forms_py" > ~/dev/$appName/$modelName/forms.py
+
+# allow hosting locally and on pythonanywhere
+allowed_hosts="ALLOWED_HOSTS = ['andrewmcloughlin.pythonanywhere.com','127.0.0.1']"
+sed -i "s/^ALLOWED_HOSTS.*/${allowed_hosts}/" ~/dev/$appName/$appName/settings.py
+
+# add model to admin interface   (not working)
+echo -e "$admin_text" > ~/dev/$appName/$modelName/admin.py
+
+# Create superuser; TODO Give superuser more permissions
 echo -e "\n${bold}create superuser${normal}"
 python3 manage.py createsuperuser
 
+# make migrations
+python3 manage.py makemigrations $appName
+python3 manage.py migrate
 
+#runserver
+cd ~/dev/$appName
 python3 manage.py runserver
-
-
