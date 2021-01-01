9649aa0e911439c3dc3cb993d8e016789d981934
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2021-01-01 17:32:07 +0000

    add html templates

--- django-app-wizard.sh
+++ django-app-wizard.sh
@@ -44,17 +44,17 @@ views_py="
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from $modelName.models import $modelName
-from $modelName.forms import modelNameForm
+from .models import $modelName
+from .forms import mainForm
 
 def list_view(request):
     all_objects = $modelName.objects.all()
     context = {
         'all_objects': all_objects
     }
-    return render(request, '$modelName/index.html', context)
+    return render(request, '$appName/index.html', context)
 
-def main_form(request):
+def add(request):
     form = mainForm()
     if request.method == 'POST':
         form = mainForm(request.POST)
@@ -64,13 +64,13 @@ def main_form(request):
     context = {
         'form': form
     }
-    return render(request, '$modelName/add.html', context)
+    return render(request, '$appName/add.html', context)
 "
 forms_py="
 from django.forms import ModelForm
-from $modelName.models import $modelName
+from .models import $modelName
 
-class modelNameForm(ModelForm):
+class mainForm(ModelForm):
     class Meta:
         model = $modelName
         fields = '__all__'
@@ -86,13 +86,89 @@ admin.site.register(testModel)
 urls_py="
 from django.contrib import admin
 from django.urls import path
-#from $modelName.views import list_view             #cannot seem to get this working
+from $appName.views import list_view, add
 
 urlpatterns = [
-    #path('', list_view),
+    path('', list_view),
+    path('add/', add),
     path('admin/', admin.site.urls),
 ]
 "
+
+base_html="
+<html>
+    <head>
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'>
+        <link rel='stylesheet' href='https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css' integrity='sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX' crossorigin='anonymous'>
+        <title>
+            $appName
+        </title>
+    </head>
+    <body>
+        <ul class='nav nav-tabs navbar-dark box-shadow'>
+            <li class='nav-item'>
+                <a class='nav-link' href='/'>$appName</a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link' href='/'>See all</a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link' href='/add'>add</a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link' href='/admin'>admin</a>
+            </li>
+        </ul>
+        <div class='container'>
+            <div class='my-3 p-3 rounded box-shadow'>
+                {% block content %}
+                {% endblock %}
+            </div>
+        </div>
+    </body>
+</html>
+"
+
+index_html="
+{% extends '$appName/base.html' %}
+{% load static %}
+{% block content %}
+{% for object in all_objects %}
+<div class='col s1 m1'>
+    <div class='card mb-4 box-shadow'>
+        <div class='card-body'>
+            <h2 class='card-text'>{{object}}</h2>
+            <p class='card-text'>Title: {{object.title}}</p>
+            <p class='card-text'>Description: {{object.description}}</p>
+            <div class='d-flex justify-content-between align-items-center'>
+                <div class='btn-group'>
+                    <button type='button' class='btn btn-sm btn-outline-secondary'>View</button>
+                    <button type='button' class='btn btn-sm btn-outline-secondary'>Edit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{% endfor %}
+<a class='btn btn-lg btn-primary' href='/add'><i class='material-icons'>add</i></a>
+{% endblock %}
+"
+
+add_html="
+{% extends '$appName/base.html' %}
+{% block content %}
+<form action='' method='post'>
+    <div class='form-group'>
+        {% csrf_token %}
+        {{form}}
+    </div>
+    <input type='submit' value='submit and return' class='btn btn-lg btn-primary'>
+    <input type='submit' value='submit and add another' class='btn btn-lg btn-secondary'>
+</form>
+{% endblock content %}
+"
+
+
 
 # set up venv
 python3 -m venv django-venv
@@ -111,8 +187,8 @@ django-admin startproject $appName
 # startapp and create model
 echo -e "\n${bold}creating a model called $modelName${normal}"
 cd $appName
-python3 manage.py startapp $modelName
-echo -e "$models_py" > ~/dev/$appName/$modelName/models.py
+# python3 manage.py startapp $modelName                              #I don't think this is necessary as startproject creates an app anyway
+echo -e "$models_py" > ~/dev/$appName/$appName/models.py
 
 # add app to installed apps
 installed_apps="INSTALLED_APPS = [\n    '${appName}',"
@@ -123,29 +199,40 @@ python3 manage.py makemigrations $appName
 python3 manage.py migrate
 
 # Add a default view
-echo -e "$views_py" > ~/dev/$appName/$modelName/views.py
+echo -e "$views_py" > ~/dev/$appName/$appName/views.py
 
 # Map URLS
 echo -e "$urls_py" > ~/dev/$appName/$appName/urls.py
 
 # Add a form
-echo -e "$forms_py" > ~/dev/$appName/$modelName/forms.py
+echo -e "$forms_py" > ~/dev/$appName/$appName/forms.py
 
 # allow hosting locally and on pythonanywhere
 allowed_hosts="ALLOWED_HOSTS = ['andrewmcloughlin.pythonanywhere.com','127.0.0.1']"
 sed -i "s/^ALLOWED_HOSTS.*/${allowed_hosts}/" ~/dev/$appName/$appName/settings.py
 
-# add model to admin interface   (not working)
-echo -e "$admin_py" > ~/dev/$appName/$modelName/admin.py
+# add model to admin interface
+echo -e "$admin_py" > ~/dev/$appName/$appName/admin.py
 
-tree -L 3
+tree -L 2
 
-# Create superuser; TODO Give superuser more permissions
+# Create superuser
 python3 manage.py createsuperuser
 
 # make migrations
 python3 manage.py makemigrations $appName
 python3 manage.py migrate
+
+# html templates
+cd ~/dev/$appName/$appName
+mkdir templates
+cd templates
+mkdir $appName
+cd $appName
+echo -e "$base_html" > ~/dev/$appName/$appName/templates/$appName/base.html
+echo -e "$index_html" > ~/dev/$appName/$appName/templates/$appName/index.html
+echo -e "$add_html" > ~/dev/$appName/$appName/templates/$appName/add.html
+
 
 #runserver
 cd ~/dev/$appName
